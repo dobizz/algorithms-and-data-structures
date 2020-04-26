d9e94e93d2ebac8d00c708134ef6ed715ae536de
Ronnie Villanueva <ronnie.code@outlook.com>
2020-04-26 17:34:45 +0600

    Update test_factorial.py

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -3,26 +3,26 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import factorial
 
-def factorial_of_zero():
+def test_factorial_of_zero():
     n = 0
     assert factorial.factorial_recursion(n) == 1
     assert factorial.factorial_recursion_memo(n) == 1
     assert factorial.factorial_iter(n) == 1
 
-def factorial_of_one():
+def test_factorial_of_one():
     n = 1
     assert factorial.factorial_recursion(n) == 1
     assert factorial.factorial_recursion_memo(n) == 1
     assert factorial.factorial_iter(n) == 1
     
-def factorial_of_10():
+def test_factorial_of_10():
     n = 10
     ans = 3628800
     assert factorial.factorial_recursion(n) == ans
     assert factorial.factorial_recursion_memo(n) == ans
     assert factorial.factorial_iter(n) == ans
     
-def factorial_of_neg():
+def test_factorial_of_neg():
     n = -1
     ans = None
     assert factorial.factorial_recursion(n) == ans
@@ -32,8 +32,8 @@ def factorial_of_neg():
     
     
 if __name__ == '__main__':
-    factorial_of_zero()
-    factorial_of_one()
-    factorial_of_10()
-    factorial_of_neg()
+    test_factorial_of_zero()
+    test_factorial_of_one()
+    test_factorial_of_10()
+    test_factorial_of_neg()
     
